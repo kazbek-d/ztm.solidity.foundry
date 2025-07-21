91167649ab7ebd47a4264ba0f3e89c6d4e39636d
Kazbek Dzarasov <kazbek_d@hotmail.com>
2025-07-21 11:31:01 +0200

    Added test

--- my_erc20/test/ERC20.t.sol
+++ my_erc20/test/ERC20.t.sol
@@ -6,6 +6,19 @@ import {ERC20} from "../src/ERC20.sol";
 
 contract Erc20Test is ERC20, Test {
 
+  address private alice;
+  address private bob;
+
   constructor() ERC20("name", "SYM", 18) {}
+
+  function setUp() public {
+    alice = makeAddr("alice");
+    bob = makeAddr("bob");
+  }
+
+  function testTransferTokensCorrectly() public virtual {
+    vm.prank(alice);
+    this.transfer(bob, 100);
+  }
 
 }
