81f4d13a9926de0f3b2acc12eb0198eb345b6e4f
Anzhela DZARASOVA <angelas82@list.ru>
2025-07-24 08:44:23 +0200

    Refactors formatting and test setup

--- my_erc20/src/ERC20.sol
+++ my_erc20/src/ERC20.sol
@@ -5,14 +5,15 @@ contract ERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address spender, uint256 value);
 
-    address public constant feeAddress = address(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
+    address public constant feeAddress =
+        address(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
 
     string public name; // ERC20
     string public symbol; // ERC20
-    uint8 immutable public decimals; // 18
+    uint8 public immutable decimals; // 18
 
     uint256 internal _totalSupply;
-    
+
     mapping(address => uint256) public balancesOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
@@ -22,9 +23,16 @@ contract ERC20 {
         decimals = _decimals;
     }
 
-    function _transfer(address from, address to, uint256 amount) private returns(bool) {
-        require(balancesOf[from] >= amount, "ERC20: Insufficient sender balance");
-        
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) private returns (bool) {
+        require(
+            balancesOf[from] >= amount,
+            "ERC20: Insufficient sender balance"
+        );
+
         uint256 fee = amount / 100;
 
         emit Transfer(from, to, amount - fee);
@@ -37,26 +45,36 @@ contract ERC20 {
         return true;
     }
 
-    function transfer(address recipient, uint256 amount) external returns (bool) {
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool) {
         return _transfer(msg.sender, recipient, amount);
     }
 
-    function transferFrom(address sender, address recipient, uint256 amount) external returns(bool) {
-        require(allowance[sender][msg.sender] >= amount, "ERC20: Insufficient allowance");
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool) {
+        require(
+            allowance[sender][msg.sender] >= amount,
+            "ERC20: Insufficient allowance"
+        );
         allowance[sender][msg.sender] -= amount;
         emit Approval(sender, msg.sender, allowance[sender][msg.sender]);
         return _transfer(sender, recipient, amount);
     }
 
-    function approve(address spender, uint256 amount) external returns (bool){
+    function approve(address spender, uint256 amount) external returns (bool) {
         allowance[msg.sender][spender] += amount;
         emit Approval(msg.sender, spender, amount);
         return true;
-    } 
+    }
 
     function _mint(address to, uint256 amount) internal {
         balancesOf[to] += amount;
-        _totalSupply +=amount;
+        _totalSupply += amount;
 
         emit Transfer(address(0), to, amount);
     }
@@ -67,5 +85,4 @@ contract ERC20 {
 
         emit Transfer(from, address(0), amount);
     }
-
 }
--- my_erc20/test/ERC20.t.sol
+++ my_erc20/test/ERC20.t.sol
@@ -5,24 +5,24 @@ import {Test} from "forge-std/Test.sol";
 import {ERC20} from "../src/ERC20.sol";
 
 contract Erc20Test is ERC20, Test {
+    address private alice;
+    address private bob;
 
-  address private alice;
-  address private bob;
+    constructor() ERC20("name", "SYM", 18) {}
 
-  constructor() ERC20("name", "SYM", 18) {}
+    function setUp() public {
+        alice = makeAddr("alice");
+        bob = makeAddr("bob");
 
-  function setUp() public {
-    alice = makeAddr("alice");
-    bob = makeAddr("bob");
+        _mint(alice, 300e18);
+    }
 
-    _mint(alice, 200e18);
-  }
+    function testTransferTokensCorrectly() public virtual {
+        vm.prank(alice);
+        bool success = this.transfer(bob, 100e18);
+        assertTrue(success, "Transfer should succeed");
 
-  function testTransferTokensCorrectly() public virtual {
-    vm.prank(alice);
-    bool success = this.transfer(bob, 100e18);
-    assertTrue(success, "Transfer should succeed");
-    assertEq(balancesOf[alice], 100e18, "Aalice's balance should be 100 after transfer");
-  }
-
+        assertEqDecimal(this.balancesOf(alice), 100e18, decimals);
+        assertEqDecimal(this.balancesOf(bob), 99e18, decimals);
+    }
 }
