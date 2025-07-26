1a38e14ce86cd760e347b0f2414f103c5905f2c7
Anzhela DZARASOVA <angelas82@list.ru>
2025-07-26 08:50:14 +0200

    Refactors test structure and updates token balances

--- my_erc20/test/ERC20.t.sol
+++ my_erc20/test/ERC20.t.sol
@@ -1,20 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console2, StdStyle} from "forge-std/Test.sol";
 import {ERC20} from "../src/ERC20.sol";
 
-contract Erc20Test is ERC20, Test {
-    address private alice;
-    address private bob;
+contract BaseSetup is ERC20, Test {
+    address internal alice;
+    address internal bob;
 
     constructor() ERC20("name", "SYM", 18) {}
 
-    function setUp() public {
+    function setUp() public virtual {
         alice = makeAddr("alice");
         bob = makeAddr("bob");
 
+        console2.log(StdStyle.blue("When Alice has 300e18 Tokens"));
         _mint(alice, 300e18);
+
+        // TODO: check why deal is not working
+        // deal(address(this), alice, 300e18);
+    }
+}
+
+contract Erc20TransferTest is BaseSetup {
+    function setUp() public override {
+        BaseSetup.setUp();
+        _mint(bob, 100e18);
     }
 
     function testTransferTokensCorrectly() public virtual {
@@ -22,7 +33,7 @@ contract Erc20Test is ERC20, Test {
         bool success = this.transfer(bob, 100e18);
         assertTrue(success, "Transfer should succeed");
 
-        assertEqDecimal(this.balancesOf(alice), 100e18, decimals);
-        assertEqDecimal(this.balancesOf(bob), 99e18, decimals);
+        assertEqDecimal(this.balancesOf(alice), 200e18, decimals);
+        assertEqDecimal(this.balancesOf(bob), 199e18, decimals);
     }
 }
